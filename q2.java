19cee35fb0c4877a76997d768dbda44bd2f98138
UserLasa73 <lasanthakumaraofficial@gmail.com>
2025-03-20 23:06:51 +0530

    Add code to print numbers from 1 to 10

---
+++ q2.java
@@ -0,0 +1,12 @@
+//Q2.Print numbers from 1 to 10
+
+import java.util.*;
+
+public class q2{
+	public static void main(String args[]){
+		for(int i=1;i<10;i++){
+			System.out.println(i);
+		}
+		
+	}
+}
